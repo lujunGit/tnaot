9306a02434a4c3870cd5d064d2e9652f50240f3b
lujun <198978176@qq.com>
2018-04-09 00:45:43 +0800

    完善shiro权限认证

--- shiroAuth-service/src/main/java/com/mongcent/tnaot/api/v1/UserController.java
+++ shiroAuth-service/src/main/java/com/mongcent/tnaot/api/v1/UserController.java
@@ -4,99 +4,124 @@ import com.mongcent.tnaot.model.Permission;
 import com.mongcent.tnaot.model.Role;
 import com.mongcent.tnaot.model.User;
 import com.mongcent.tnaot.service.UserServiceV1;
+
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Random;
 
-@RestController
-@RequestMapping("/sys/user/v1")
-@ResponseBody
+@Controller
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserServiceV1 userServiceV1;
 
-    /**
-     * 登出
-     * @return
-     */
     @GetMapping("/login")
-    public Object login(){
+    public String login() {
+        logger.info("request input getLogin");
         return "login";
     }
 
     /**
      * 登入
-     * @return
      */
     @PostMapping("/login")
-    public Object login(@RequestBody Map map){
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(map.get("userName").toString(), map.get("password").toString());
-        subject.login(usernamePasswordToken);
-        return "login";
+    public Object login(Model model, String userName, String password) {
+        UsernamePasswordToken usernamePasswordToken = null;
+        Subject subject = null;
+        try {
+            subject = SecurityUtils.getSubject();
+            usernamePasswordToken = new UsernamePasswordToken(userName, password);
+            subject.login(usernamePasswordToken);
+            logger.info("账号：" + userName + "-密码：" + password + "sessionId:" + subject.getSession().getId());
+        } catch (IncorrectCredentialsException e) {
+            model.addAttribute("msg", "密码错误");
+        } catch (LockedAccountException e) {
+            model.addAttribute("msg", "登录失败，该用户已被冻结");
+        } catch (AuthenticationException e) {
+            model.addAttribute("msg", "该用户不存在");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //验证是否登录成功
+        if (subject.isAuthenticated()) {
+            logger.info("用户[" + userName + "]登录认证通过(这里可以进行一些认证通过后的一些系统参数初始化操作)");
+            model.addAttribute("msg", "用户【"+userName+"】登陆成功！sessionId: "+subject.getSession().getId());
+            return "index";
+        } else {
+            usernamePasswordToken.clear();
+            return "login";
+        }
     }
 
-    @RequestMapping(value = "/index")
-    public String index(){
+    @GetMapping("/index")
+    public String index() {
         return "index";
     }
 
     //登出
-    @RequestMapping(value = "/logout")
-    public String logout(){
+    @GetMapping("/logout")
+    public String logout() {
+        /*SecurityUtils.getSubject().logout();
+        redirectAttributes.addFlashAttribute("message", "您已安全退出");*/
         return "logout";
     }
 
     //错误页面展示
-    @RequestMapping(value = "/error",method = RequestMethod.POST)
-    public String error(){
-        return "error ok!";
+    @GetMapping("/error")
+    public String error() {
+        return "error";
     }
 
     //数据初始化
-    @RequestMapping(value = "/addUser")
-    public String addUser(){
+    @GetMapping("/addUser")
+    public String addUser() {
         Permission permission1 = new Permission();
         permission1.setPermission("create");
         Permission permission2 = new Permission();
         permission2.setPermission("update");
 
         Role role = new Role();
-        role.setPermissions(Arrays.asList(permission1,permission2));
+        role.setPermissions(Arrays.asList(permission1, permission2));
         role.setRoleName("admin");
 
-        User user  = new User();
+        User user = new User();
         user.setName("aaa");
         user.setPassword(123456);
         user.setRoles(Arrays.asList(role));
 
-        User userInfo =userServiceV1.addUser(user);
+        User userInfo = userServiceV1.addUser(user);
         return "addUser is ok! \n" + userInfo;
     }
 
     //角色初始化
-    @RequestMapping(value = "/addRole")
-    public String addRole(){
+    @GetMapping("/addRole")
+    public String addRole() {
         Permission permission1 = new Permission();
         permission1.setPermission("create");
         Permission permission2 = new Permission();
         permission2.setPermission("update");
 
         Role role = new Role();
-        String roleName = "role_"+(new Random().nextInt(100) + 1);
+        String roleName = "role_" + (new Random().nextInt(100) + 1);
         role.setRoleName(roleName);
-        role.setPermissions(Arrays.asList(permission1,permission2));
+        role.setPermissions(Arrays.asList(permission1, permission2));
         Role roleInfo = userServiceV1.addRole(role);
         return "addRole is ok! \n" + roleInfo;
     }
@@ -104,14 +129,19 @@ public class UserController {
     //注解的使用
     @RequiresRoles("admin")
     @RequiresPermissions("create")
-    @RequestMapping(value = "/create")
-    public String create(){
+    @GetMapping("/create")
+    public String create() {
         return "Create success!";
     }
 
     @GetMapping("/view/page")
-    public String view(Model model){
-        model.addAttribute("name","dajun");
+    public String view(Model model) {
+        model.addAttribute("name", "dajun");
         return "hello";
+    }
+
+    //设置用户登陆次数
+    public int setUserLoginNum(){
+        return 0;
     }
 }
--- shiroAuth-service/src/main/java/com/mongcent/tnaot/auth/MyShiroRealm.java
+++ shiroAuth-service/src/main/java/com/mongcent/tnaot/auth/MyShiroRealm.java
@@ -4,6 +4,7 @@ import com.mongcent.tnaot.model.Permission;
 import com.mongcent.tnaot.model.Role;
 import com.mongcent.tnaot.model.User;
 import com.mongcent.tnaot.service.UserServiceV1;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -12,9 +13,13 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MyShiroRealm extends AuthorizingRealm {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthorizingRealm.class);
 
     //用于用户查询
     @Autowired
@@ -24,17 +29,22 @@ public class MyShiroRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         //获取登录用户名
-        String name= (String) principalCollection.getPrimaryPrincipal();
+        String name = (String) principalCollection.getPrimaryPrincipal();
+
+        logger.info("登陆账号：" + name);
+
         //查询用户名称
         User user = userServiceV1.getUserByName(name);
         //添加角色和权限
         SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
-        for (Role role:user.getRoles()) {
+        for (Role role : user.getRoles()) {
             //添加角色
             simpleAuthorizationInfo.addRole(role.getRoleName());
-            for (Permission permission:role.getPermissions()) {
+            logger.info("账号角色：" + role.getRoleName());
+            for (Permission permission : role.getPermissions()) {
                 //添加权限
                 simpleAuthorizationInfo.addStringPermission(permission.getPermission());
+                logger.info("角色权限：" + permission.getPermission());
             }
         }
         return simpleAuthorizationInfo;
@@ -42,7 +52,8 @@ public class MyShiroRealm extends AuthorizingRealm {
 
     //用户认证
     @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+    protected AuthenticationInfo doGetAuthenticationInfo(
+            AuthenticationToken authenticationToken) throws AuthenticationException {
         //加这一步的目的是在Post请求的时候会先进认证，然后在到请求
         if (authenticationToken.getPrincipal() == null) {
             return null;
@@ -50,12 +61,16 @@ public class MyShiroRealm extends AuthorizingRealm {
         //获取用户信息
         String name = authenticationToken.getPrincipal().toString();
         User user = userServiceV1.getUserByName(name);
+        logger.info("认证账号：" + name);
+
+        //int lockRandom = new Random().nextInt(1);
         if (user == null) {
             //这里返回后会报出对应异常
             return null;
-        } else {
+        }else{
             //这里验证authenticationToken和simpleAuthenticationInfo的信息
-            SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(name, user.getPassword().toString(), getName());
+            SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(name,
+                    user.getPassword().toString(), getName());
             return simpleAuthenticationInfo;
         }
     }
